9452e665d77827939c70af4720497cea996590fc
JamesDrysdale <jamesdrysdale84@gmail.com>
2020-04-24 16:28:22 +0100

    Create tables

---
+++ db/pokecenter.sql
@@ -0,0 +1,42 @@
+DROP TABLE medical_records;
+DROP TABLE pokemon;
+DROP TABLE owner;
+DROP TABLE vet;
+
+
+CREATE TABLE vet
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE owner 
+(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    phone INT,
+    email VARCHAR(255) UNIQUE,
+    address TEXT
+);
+
+CREATE TABLE pokemon
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    dob VARCHAR(255),
+    type VARCHAR(255),
+    affinity VARCHAR(255),
+    level INT,
+    allergies TEXT,
+    owner_id INT REFERENCES owner(id)
+);
+
+CREATE TABLE medical_records
+(
+    id SERIAL PRIMARY KEY,
+    pokemon_id INT REFERENCES pokemon(id),
+    vet_id INT REFERENCES vet(id),
+    date VARCHAR(255), -- Treatment date
+    details TEXT  -- Treatment details
+);
